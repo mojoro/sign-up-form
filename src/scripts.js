9e7b341f8900b314777ef2fe8023d62bc001fc96
mojoro <45500240+mojoro@users.noreply.github.com>
2024-03-25 20:11:09 +0100

    Finalize styling and add password match message

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,9 @@
 const password = document.getElementById('password');
 const passwordConfirm = document.getElementById('confirm-pass');
 
+const passParent = document.querySelector('.pass-parent');
+const passConfirmParent = document.querySelector('.confirm-parent');
+
 function validatePassword() {
   if (password.value !== passwordConfirm.value) {
     passwordConfirm.setCustomValidity("Passwords Don't Match");
@@ -8,12 +11,16 @@ function validatePassword() {
     password.classList.add('error');
     passwordConfirm.classList.remove('success');
     password.classList.remove('success');
+
+    passParent.classList.add('error-parent');
   } else {
     passwordConfirm.setCustomValidity('');
     passwordConfirm.classList.remove('error');
     password.classList.remove('error');
     passwordConfirm.classList.add('success');
     password.classList.add('success');
+
+    passParent.classList.remove('error-parent');
   }
 }
 
